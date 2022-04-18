37ddfe44369c0a7d0a8f10e7c811e42e824a3149
hiranmai19 <91332430+hiranmai19@users.noreply.github.com>
2022-04-18 23:17:00 +0530

    Update Memory.py

--- Phase1/Memory.py
+++ Phase1/Memory.py
@@ -1,1 +1,23 @@
+def command(a, n):
+    x = len(a)
+    if x == n:
+        return a
+    else:
+        p = a[::-1]
+        # i=0
+        for y in range(0, n-x):
+            p = p + '0'
+        p = p[::-1]
+        return p
+class Memory:
+    data = {}
+    text = []
+    stack = []
+    pointer = 268435456
+
+    def add_details(s, add, val):
+        # add = hex(add).zfill(8)
+        add = '0x'+(hex(add)[2::].zfill(8))
+        s.data[add] = hex(int(val, 2)).replace('0x', '').zfill(2)
+        
 
