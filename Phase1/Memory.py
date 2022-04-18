4b27ba0edc34e2bf7556c5ce1f13465279921ce9
Akshara-333 <91304813+Akshara-333@users.noreply.github.com>
2022-04-18 23:17:44 +0530

    Update Memory.py

--- Phase1/Memory.py
+++ Phase1/Memory.py
@@ -20,4 +20,59 @@ class Memory:
         add = '0x'+(hex(add)[2::].zfill(8))
         s.data[add] = hex(int(val, 2)).replace('0x', '').zfill(2)
         
+def adddata(s, type, val):
+        if(type != '.asciiz'):
+            val = val.replace(",", ' ')
+        arr = val.split(' ')
+        myreturnvalue = len(s.data)
+        
+        if(type == '.word'):
+            ret_value = s.pointer
+            for x in arr:
+                if(len(x) > 1):  # NO need to convert if passed value is already in hex
+                    if(x[0] == '0' and x[1] == 'x'):
+                        if(len(x) <= 10):
+                            y = command(x[2::], 8)
+                        else:
+                            print("value", x, " too large to fit in a word, storing 4 least sign. bytes")
+                            y=x[2::][-8:]
+                    else:  # not hex
+                        y = hex(int(x) & 0xffffffff)[2::].zfill(8)
+                        # print(y)
+                        if(len(y) <= 8):
+                            y = command(y, 8)
+                        else:
+                            print("can't store", x, "in a word, because value is too large. truncating the value and storing 4 least significant bytes")
+                            y = y[-8:]
+                else:  # length <=1
+                    y = hex(int(x.strip()))[2::]
+                    y = command(y, 8)
+                y1 = y[0:2]
+                y2 = y[2:4]
+                y3 = y[4:6]
+                y4 = y[6:8]
+                s.data[hex(s.pointer)] = (y4)
+                s.pointer += 1
+                s.data[hex(s.pointer)] = (y3)
+                s.pointer += 1
+                s.data[hex(s.pointer)] = (y2)
+                s.pointer += 1
+                s.data[hex(s.pointer)] = (y1)
+                s.pointer += 1  
+            return hex(ret_value)   
+        
+        elif(type == '.asciiz'):
+            ret_value = s.pointer
+            for x in range(len(val)):
+                s.data[hex(s.pointer)] = (hex(ord(val[x]))[2::])
+                s.pointer+=1
+            s.data[hex(s.pointer)] = '00'
+            s.pointer+=1
+            return hex(ret_value)
+        else:
+            print("Unrecognized datatype directive",type)
+            return
+        	# print(val,"stored at =" ,hex(myreturnvalue+268435456))
+        	# print(self.data)
+        	# return hex(int(myreturnvalue+268435456))
 
